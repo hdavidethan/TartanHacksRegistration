c3bf8df44e91a329dd117aa69b4c7b03f81c29b2
Richard G <rwguo@andrew.cmu.edu>
2018-12-25 19:00:32 -0600

    changes to application and confirmation

--- app/server/models/User.js
+++ app/server/models/User.js
@@ -16,7 +16,7 @@ var profile = {
   adult: {
     type: Boolean,
     required: true,
-    default: false,
+    default: true,
   },
 
   school: {
@@ -28,7 +28,7 @@ var profile = {
   graduationYear: {
     type: String,
     enum: {
-      values: '2016 2017 2018 2019'.split(' '),
+      values: '2019 2020 2021 2022'.split(' '),
     }
   },
 
